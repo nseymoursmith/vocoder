8af1a5f13167d925df26706f66709074b00342da
nseymoursmith <nseymoursmith@gmail.com>
2016-01-29 16:52:55 +0000

    Added multi-stage recursive bpf - two stages seems sufficient. good synth and presets. Only need to add a compressor

--- src/vocoder/core.clj
+++ src/vocoder/core.clj
@@ -2,43 +2,59 @@
   (:use [overtone.live]
         [overtone.inst.synth]))
 
-(defn- log-spacing
-  [fmin fmax nbands]
-  (let [base (pow (/ fmax fmin) (/nbands))
-        n (range nbands)]
-    (map #(* fmin (pow base (+ % 1))) n)))
+(defn- nbpf
+  ; n-stage recursive band-pass filter
+  [input freq rq stages]
+  (loop [n stages signal input]
+    (if (zero? n) signal
+        (recur (dec n) (bpf signal freq rq)))))
 
 (defsynth vocoder
   [bus 0
    mic-input 8
    fmin 20.0
-   fmax 20000.0
-   nbands 10
-   rq-voc 0.65
-   rq-synth 0.65
-   distance 1.0
+   fmax 10000.0
+   nbands 9
+   stages 2
+   rq-voc 0.32
+   rq-synth 0.32
+   distance 10.0
    voc-gain 1.0
    src-gain 1.0
    comp-gain 1.0
    fsib 8000.0
-   sib-gain 1.0
-   gain 1.0]
+   sib-gain 0.001
+   gain 140.0]
   (let [; - sources - ;
         src (in bus)
         input (in mic-input)
 
         ; - mic frequency band amplitudes -
         voc (* voc-gain input)
-        band-freqs (log-spacing fmin fmax (. nbands value))
-        voc-bands (map #(bpf voc % rq-voc) band-freqs)
-        voc-amps (map (fn [band center] 
-                        (let [tau (* distance (/ center))]
-                          (amplitude band tau tau)))
-                      voc-bands band-freqs)
+        voc-level (amplitude:kr voc 0.1 0.1)
+        fmin* (. fmin value)
+        fmax* (. fmax value)
+        nbands* (. nbands value)
+        base (Math/pow (/ fmax* fmin*) (/ nbands*))
+        n (range nbands*)
+        band-freqs (map #(* fmin* (Math/pow base (+ % 1))) n)
+        stages* (. stages value)
+        voc-bands (map #(nbpf voc % rq-voc stages*) band-freqs)
+        ;; voc-bands (map #(bpf voc % rq-voc) band-freqs)
+        ;; voc-bands (map (fn [band centre] (bpf band centre rq-synth)) 
+        ;;                voc-bands band-freqs)
+        taus (map #(/ distance %) band-freqs)
+        voc-amps (map (fn [band tau] (amplitude band tau tau)) 
+                      voc-bands taus)
+;;        voc-amps (map #(/ % voc-level) voc-amps)
+        
 
         ; - extract and mix src bands -
         src (* src-gain src)
-        src-bands (map #(bpf src % rq-synth) band-freqs)
+        src-level (amplitude:kr src 0.1 0.1)
+        src-bands (map #(nbpf src % rq-synth stages*) band-freqs)
+        ;; src-bands (map (fn [band centre] (bpf band centre rq-synth)) 
+        ;;                src-bands band-freqs)
         src-voc-mix (map * src-bands voc-amps)
         composited (* comp-gain (sum src-voc-mix))
 
@@ -49,97 +65,59 @@
         formed (pv-mul (fft a input) (fft b noise-source))
         sib-synth (ifft formed)
         sibilance (hpf sib-synth fsib)
-        src-level (amplitude:kr src)
         sibilance (* sib-gain src-level sibilance)
 
-        output (sum [composited sibilance])]
+        output (sum [composited sibilance])
+        ]
     (replace-out bus (* gain output))))
 
-(definst cs80lead-midi
-  [note 69
-   amp 0.5
-   gate 1
-   att 0.75
-   decay 0.5
-   sus 0.8
-   rel 1.0
-   fatt 0.75
-   fdecay 0.5
-   fsus 0.8
-   frel 1.0
-   cutoff 200
-   dtune 0.002
-   vibrate 4
-   vibdepth 0.015
-   ratio 1
-   cbus 1
-   freq-lag 0.1]
-  (let [freq (midicps note)
-        freq (lag freq freq-lag)
-        cuttoff (in:kr cbus)
-        env     (env-gen (adsr att decay sus rel) gate :action FREE)
-        fenv    (env-gen (adsr fatt fdecay fsus frel 2) gate)
-
-        vib     (+ 1 (lin-lin:kr (sin-osc:kr vibrate) -1 1 (- vibdepth) vibdepth))
-
-        freq    (* freq vib)
-        sig     (mix (* env amp (saw [freq (* freq (+ dtune 1))])))]
-    sig))
-
-;;Start midi-controlled vocoder with presets
 (do
-  (def voco (inst-fx! cs80lead-midi vocoder))
-  (def voc-player (midi-poly-player cs80lead-midi))
+  (def voco (inst-fx! pad2 vocoder))
+  (def voc-player (midi-poly-player pad2))
 )
-
 ;;Stop midi-controlled vocoder
 (do
+  (clear-fx pad2)
   (midi-player-stop voc-player)
-  (clear-fx cs80lead-midi)
-  (stop))
+  (stop)
+)
 
-;;preset for messing around
-(do 
-  (ctl cs80lead-midi :note 50)
-  (ctl cs80lead-midi :vibdepth 0.015)
-  (ctl cs80lead-midi :vibrate 8)
-  (ctl cs80lead-midi :sus 1)
-  (ctl voco :voc-band-gain 10)
-  (ctl voco :src-band-gain 10)
-  (ctl voco :comp-gain 1.001)
-  (ctl voco :fsib 8000)
-  (ctl voco :sib-gain 1.0010)
-  (ctl voco :rq-voc 0.15)
-  (ctl voco :rq-synth 0.15)
-  (ctl voco :distance 100)
-  (ctl voco :gain 1))
-
-;;preset 1
-(do 
-  (ctl cs80lead-midi :note 50)
-  (ctl cs80lead-midi :vibdepth 0.015)
-  (ctl cs80lead-midi :vibrate 8)
-  (ctl cs80lead-midi :sus 1)
-  (ctl voco :voc-band-gain 10)
-  (ctl voco :src-band-gain 10)
-  (ctl voco :comp-gain 1.001)
-  (ctl voco :fsib 8000)
-  (ctl voco :sib-gain 1.0010)
-  (ctl voco :rq-voc 0.15)
-  (ctl voco :rq-synth 0.15)
-  (ctl voco :distance 100)
-  (ctl voco :gain 1))
-
-
-(recording-start "~/Desktop/mic-check-09.wav")
+(recording-start "~/Desktop/mic-check-24.wav")
 (recording-stop)
 
-(cs80lead-midi :note 50)
-(ctl cs80lead-midi :gate 0)
-(pluck)
-(pm-osc)
-(stop)
-(square)
-(pulse)
-(needle-rect)
-(skip-needle)
+;;preset play
+(do 
+  (ctl voco :voc-gain 1)
+  (ctl voco :src-gain 1)
+  (ctl voco :comp-gain 1.0)
+  (ctl voco :fsib 8000)
+  (ctl voco :sib-gain 0.001)
+  (ctl voco :rq-voc 0.32)
+  (ctl voco :rq-synth 0.32)
+  (ctl voco :distance 10)
+  (ctl voco :gain 140.0))
+
+(definst pad2
+  [note 60 t 10 amt 0.3 amp 0.1 a 0.4 d 0.5 s 0.8 r 2 gate 1]
+  (let [freq   (midicps note)
+        lfo    (+ 2 (* 0.01 (sin-osc:kr 5 (rand 1.5))))
+        src    (apply + (saw [freq (* freq lfo)]))
+        env    (env-gen (adsr a d s r) gate :action FREE)
+        f-env  (x-line:kr 0.001 4 t)
+        src    (* env src)
+        signal (rlpf src (+ (* 0.3 freq) (* f-env 2 freq)) 0.5)
+        k      (/ (* 4 amt) (- 1 amt))
+        dist   (clip2 (/ (* (+ 1 k) signal) (+ 1 (* k (abs signal))))
+                      0.03)
+        snd    (* amp dist (line:kr 1 0 t))]
+    src))
+
+(defsynth bizzle [out-bus 10 amp 0.5]
+  (out out-bus
+       (* amp
+          (+ (* (decay2 (* (impulse 10 0)
+                           (+ (* (lf-saw:kr 0.3 0) -0.3) 0.3))
+                        0.001)
+                0.3)
+             (apply + (pulse [80 81]))))))
+
